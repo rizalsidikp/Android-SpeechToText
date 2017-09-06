a8ec110e67087f3a89ff11bb037b469cc4c772c8
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-06 13:48:13 +0700

    add speech to text java

--- app/src/main/java/com/example/rizalsidikp/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/rizalsidikp/speechtotext/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.rizalsidikp.speechtotext;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    Button speechButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (TextView) findViewById(R.id.result);
+        speechButton = (Button) findViewById(R.id.btnspeak);
+
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputSpeechDialog();
+            }
+        });
+
     }
+
+//  function untuk menampilkan dialog google speech input
+    private void inputSpeechDialog(){
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//      dibawah ini adalah settingan untuk memanggil google speech input
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.sesuatu));
+
+//      ini adalah perintah untuk mengeksekusi intent dan memunculkan dialog speech input google
+        try {
+            startActivityForResult(i, REQ_CODE_SPEECH_INPUT);
+        }catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), getString(R.string.tidaksupport), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//  ini function ketika menerima hasil dari dialog google speech input
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if(resultCode == RESULT_OK && null != data){ //kalau resultCode nya sama dengan OK
+//                  masukkan hasil suara kedalam array (biar bisa lebih dari 1 kata)
+                    ArrayList<String> hasil = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                  masukkan hasil array ke text
+                    result.setText(hasil.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+
 }
